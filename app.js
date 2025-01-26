0cd529470325d19a221d13fda2ee42cf281e1955
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2025-01-26 17:01:00 +0200

    Exercise 11.9. Implement e2e test using PlayWright

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
